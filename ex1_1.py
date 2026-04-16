8f280a29f603b9c9dd8ab0d7cb4504293fe0521e
manuelm11-bit <manuel.miranda.t0129866@edu.atec.pt>
2026-04-16 19:40:12 +0100

    Delete ex1_1.py

--- ex1_1.py
+++
@@ -1,9 +0,0 @@
-dia = input("Digite o dia da semana: ").strip().lower()
-
-match dia:
-    case "sábado" | "sabado" | "domingo":
-        print("Fim de semana")
-    case "segunda" | "terça" | "quarta" | "quinta" | "sexta":
-        print("Dia útil")
-    case _:
-        print("Dia inválido")
