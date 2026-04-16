f0853839998f384df27ffd587cbafc1a3b5222f6
manuelm11-bit <manuel.miranda.t0129866@edu.atec.pt>
2026-04-16 19:34:38 +0100

    Delete ex2_4.py

--- ex2_4.py
+++
@@ -1,8 +0,0 @@
-saldo = float(input("Saldo inicial: "))
-cheque = float(input("Valor do cheque: "))
-
-if cheque <= saldo:
-    saldo_atual = saldo - cheque
-    print(f"Cheque descontado, saldo: {saldo_atual:.0f}")
-else:
-    print("Cheque não pode ser descontado")
