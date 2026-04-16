36cd8f2462c19510904bc9b493aa5b25e3608016
manuelm11-bit <manuel.miranda.t0129866@edu.atec.pt>
2026-04-16 19:40:36 +0100

    Delete ex1_3.py

--- ex1_3.py
+++
@@ -1,9 +0,0 @@
-pedido = eval(input("Digite o dicionário: "))  # Ex: {"tipo": "venda", "valor": 250}
-
-match pedido:
-    case {"tipo": "compra", "valor": valor}:
-        print(f"Compra de {valor}€")
-    case {"tipo": "venda", "valor": valor}:
-        print(f"Venda de {valor}€")
-    case _:
-        print("Pedido desconhecido")
