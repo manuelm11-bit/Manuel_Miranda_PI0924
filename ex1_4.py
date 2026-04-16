d97fc4f057001d4f215972f0b35aba7f30d441b5
manuelm11-bit <manuel.miranda.t0129866@edu.atec.pt>
2026-04-16 19:41:21 +0100

    Delete ex1_4.py

--- ex1_4.py
+++
@@ -1,9 +0,0 @@
-valor = eval(input("Digite o valor: "))
-
-match valor:
-    case int():          print("Número inteiro")
-    case float():        print("Número decimal")
-    case str() if valor.isdigit(): print("String numérica")
-    case str():          print("String textual")
-    case list():         print("Lista")
-    case _:              print("Tipo desconhecido")
