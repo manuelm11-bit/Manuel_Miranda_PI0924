f55307e36eb372c3d7746a8d8b88d4a7cf476d8d
manuelm11-bit <manuel.miranda.t0129866@edu.atec.pt>
2026-04-16 19:15:59 +0100

    Delete Exercício Loop.py

--- Exercício Loop.py
+++
@@ -1,7 +0,0 @@
-numeros = list(map(int, input("Digite 10 números separados por vírgula: ").split(',')))
-
-pares = sum(1 for n in numeros if n % 2 == 0)
-impares = len(numeros) - pares
-
-print(f"Pares: {pares}")
-print(f"Ímpares: {impares}")
