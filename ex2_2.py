b239804bd9ba614c4e307c69fb74c4923ea92fa4
manuelm11-bit <manuel.miranda.t0129866@edu.atec.pt>
2026-04-16 19:21:22 +0100

    Delete ex2_2.py

--- ex2_2.py
+++
@@ -1,9 +0,0 @@
-num1 = int(input("num1 = "))
-num2 = int(input("num2 = "))
-num3 = int(input("num3 = "))
-
-maior = max(num1, num2, num3)
-menor = min(num1, num2, num3)
-
-print(f"Maior: {maior}")
-print(f"Menor: {menor}")
