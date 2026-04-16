e3627c82fa1b9ec8d849cd7a26e113a98189cdf5
manuelm11-bit <manuel.miranda.t0129866@edu.atec.pt>
2026-04-16 19:37:31 +0100

    Delete ex3_9.py

--- ex3_9.py
+++
@@ -1,7 +0,0 @@
-while True:
-    num = int(input("Digite um número entre 1 e 100: "))
-    if 1 <= num <= 100:
-        print("Número válido!")
-        break
-    else:
-        print("Valor fora do intervalo. Tente novamente.")
